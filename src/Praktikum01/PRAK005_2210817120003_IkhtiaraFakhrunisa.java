cf7a6348802099cbb30024961e04513d94b55750
USER <ikhtiarafakhrunisa@gmail.com>
2023-09-17 10:15:58 +0800

    first commit

---
+++ src/Praktikum01/PRAK005_2210817120003_IkhtiaraFakhrunisa.java
@@ -0,0 +1,13 @@
+package Praktikum01;
+
+import java.util.Scanner;
+
+public class PRAK005_2210817120003_IkhtiaraFakhrunisa {
+
+	public static void main(String[] args) {
+	        Scanner input = new Scanner(System.in);
+	        String namaMakanan = input.nextLine();
+	        String hobi = input.nextLine();
+	        System.out.println("Aku Suka Makan " + namaMakanan + ", "  + "dan Hobiku " + hobi);
+	}
+}
