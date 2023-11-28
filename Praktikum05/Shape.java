b41595504f93eac2a156d9761bc1f5db84a86371
IkhtiaraFakhrunisa <115356707+IkhtiaraFakhrunisa@users.noreply.github.com>
2023-11-28 10:47:12 +0800

    Add files via upload

---
+++ Praktikum05/Shape.java
@@ -0,0 +1,12 @@
+package Praktikum05;
+
+public abstract class Shape {
+    private String shapeName;
+    public Shape(String shapeName) {
+        this.shapeName = shapeName;
+    }
+    public abstract double area();
+    public String toString(){
+        return this.shapeName;
+    }
+}
